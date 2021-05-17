c7e8952dda0c919b349b14b8eb9175002cd62482
Edgar Lule <edgar21lc@gmail.com>
2021-05-16 22:13:08 -0700

    updated README

---
+++ README.md
@@ -0,0 +1,26 @@
+#Description
+Updated my Portfolio page.
+
+<!-- HTML -->
+
+Added a picture of myself.
+Added Bootstrap and Jquery links to HTML
+Added bootstrap colummns to separate all the sections.
+Added Project 1 which is called Daily Vibes to webpage, also added two of the homeworks.
+Added links to Project/Homework assignemnt page and links to the repos.
+
+<!-- CSS -->
+
+Adjusted the CSS to fit this page better.
+
+<!-- Javascript -->
+
+Added a get quote button using Jquery that when clicked will pull a random quote from a premade array and it will display it on the webpage. The quotes are from hearing them in school or from podcasts that've I've listened to.
+Used the imgur API to get images from a premade album on imgur and used those images to randomly generate a new background every time the page is refreshed.
+
+<img src="./images/portfolio1.jpg" alt="Portfolio1" width="100%" height="600">
+<img src="./images/portfolio2.jpg" alt="Portfolio2" width="100%" height="600">
+<img src="./images/portfolio3.jpg" alt="Portfolio3" width="100%" height="600">
+<img src="./images/portfolio4.jpg" alt="Portfolio4" width="100%" height="600">
+
+https://edgarl21.github.io/Portfolio/
---
+++ script.js
@@ -0,0 +1,38 @@
+var quotes = [
+  "If I have seen further, it is by standing upon the shoulders of giants. - Issac Newton",
+  "Discipline equals freedom. - Jocko Willink",
+  "Don't expect to be motivated every day to get out there and make things happen. You won't be. Don’t count on motivation. Count on Discipline. - Jocko Willink",
+  "You're going to pay a price for every bloody thing you do and everything you don't do. You don't get to choose to not pay a price. You get to choose which poison you're going to take. That's it. - Jordan Peterson",
+  "If you are not willing to be a fool, you can't become a master. - Jordan Peterson",
+  "Stop researching every aspect of it and reading all about it and debating the pros and cons of it … Start doing it. - Jocko Willink",
+  "The more you practice, the better you get, the more freedom you have to create. - Jocko Willink",
+  "Most of us aren't defeated in one decisive battle. We are defeated one tiny, seemingly insignificant surrender at a time that chips away at who we should really be. - Jocko Willink",
+];
+
+$("#button").click(function () {
+  $("#quotes").text(quotes[Math.floor(Math.random() * 8)]);
+});
+
+var getWallpaper = function () {
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    "Bearer 8a4fad9f70ab2c2baf42415ee4ac88e1ebbbd653"
+  );
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+  fetch("https://api.imgur.com/3/album/jvXwfOy/images/", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      var wallpaper = result.data[Math.floor(Math.random() * 25)].link;
+      document.body.style.backgroundImage = `url(${wallpaper})`;
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+};
+
+getWallpaper();
